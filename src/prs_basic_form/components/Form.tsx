87844ae7795f7a6693e44ee5e5406b1ebbb05dad
zippy-junior <alexey@zago.ru>
2023-04-04 18:52:01 +0300

    Added initial project files

---
+++ src/prs_basic_form/components/Form.tsx
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+import { PanelProps } from '@grafana/data';
+import Form
+// import axios from 'axios';
+
+export const BasicFormPanel: React.FC<PanelProps> = ({ options, data, width, height }) => {
+  return <></>;
+};
