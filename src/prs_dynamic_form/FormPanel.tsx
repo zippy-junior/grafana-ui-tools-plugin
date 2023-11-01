d8999785ddfaf7cbea1a6355be2411a71acbfd51
zippy-junior <alexey@zago.ru>
2023-11-01 09:40:34 +0300

    #2 Changed

--- src/prs_dynamic_form/FormPanel.tsx
+++ src/prs_dynamic_form/FormPanel.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { PanelProps } from '@grafana/data';
-import { Form } from 'semantic-ui-react';
-import { IrowState } from './components/DynamicSchema/interfaces';
-import { TimeInput } from 'semantic-ui-calendar-react';
+import { Button, Form } from 'semantic-ui-react';
 import styled from '@emotion/styled';
+import axios from 'axios';
 // import axios from 'axios';
 
 const StyledForm = styled(Form)`
@@ -17,43 +16,46 @@ const StyledForm = styled(Form)`
 
 export const BasicFormPanel: React.FC<PanelProps> = ({ options, id, data, width, height }) => {
   console.log(options);
+
+  const [checkedField, setCheckedField] = useState([0, 0]);
+
+  const [tagData, setTagData] = useState(0);
+
+  useEffect(() => {
+    get_tag_data('123456');
+  }, []);
+
+  const get_tag_data = (id: string) => {
+    axios
+      .get('')
+      .then((value) => {
+        setTagData(value.data);
+        console.log(value);
+      })
+      .catch(() => {
+        console.log('Error');
+      });
+  };
+
+  function updateChecked(index, newValue) {
+    // shallow copy
+    const newArray = [...checkedField];
+    // mutate copy
+    newArray[index] = newValue;
+    // set state
+    setCheckedField(newArray);
+  }
+
   return (
     <StyledForm inverted>
-      {options.schema.map((row: IrowState, index: number) => {
-        switch (row.dataType) {
-          case 'string':
-            return (
-              <Form.Field>
-                <label>{row.fieldName}</label>
-                <input placeholder={row.fieldName} />
-              </Form.Field>
-            );
-          case 'date':
-            return (
-              <Form.Field>
-                <label>{row.fieldName}</label>
-                <TimeInput name={row.fieldName} placeholder={row.dataType} value="" onChange={() => {}} />
-              </Form.Field>
-            );
-          case 'timeInterval':
-            return (
-              <Form.Field fluid>
-                <label>{row.fieldName}</label>
-                <Form.Group widths="equal">
-                  <Form.Field fluid>
-                    <label>from</label>
-                    <TimeInput name={row.fieldName} placeholder={row.dataType} value="" onChange={() => {}} />
-                  </Form.Field>
-                  <Form.Field fluid>
-                    <label>to</label>
-                    <TimeInput name={row.fieldName} placeholder={row.dataType} value="" onChange={() => {}} />
-                  </Form.Field>
-                </Form.Group>
-              </Form.Field>
-            );
-          default:
-            return <Form.Input key={index} label={row.fieldName} />;
-        }
+      {checkedField.map((checked, i) => {
+        return (
+          <Form.Field key={i} style={{ border: checked ? '2px solid blue' : '' }}>
+            <label>{'tag name'}</label>
+            <input placeholder={tagData.toString()} />
+            <Button onClick={() => updateChecked(i, !checked[i])}>Send</Button>
+          </Form.Field>
+        );
       })}
     </StyledForm>
   );
--- src/prs_dynamic_form/components/Options.tsx
+++ src/prs_dynamic_form/components/Options.tsx
@@ -1,21 +1,6 @@
 import React from 'react';
 import { StandardEditorProps } from '@grafana/data';
-import 'semantic-ui-css/semantic.css';
-import { DynamicSchema } from './DynamicSchema/DynamicSchema';
-
-// const StyledDropdown = styled(Dropdown)`
-//   background: #434343 !important;
-//   color: #fff !important;
-//   & .menu > .item {
-//     background: #434343 !important;
-//     color: #eee !important;
-//   }
-// `;
-
-// const StyledTableRow = styled(Table.Row)`
-//   width: 100%;
-// `;
 
 export const CustomOptionsEditor: React.FC<StandardEditorProps> = ({ value, onChange }) => {
-  return <DynamicSchema optionValue={value} onOptionChange={onChange}></DynamicSchema>;
+  return <></>;
 };
